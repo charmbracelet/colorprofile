charmbracelet/colorprofile

commit 96eea4a3b253d01a9e3d39a4c66650b32dbdbaa4

fix: ignore COLORTERM when no TERM is defined
The `COLORTERM` environment variable _complements_ the `TERM` variable
and should not be taken into consideration when `TERM` is not defined.

env.go
@@ -33,8 +33,8 @@ func Detect(output io.Writer, env []string) Profile {
 	out, ok := output.(term.File)
 	environ := newEnviron(env)
 	isatty := isTTYForced(environ) || (ok && term.IsTerminal(out.Fd()))
-	term := environ.get("TERM")
-	isDumb := term == dumbTerm
+	term, ok := environ.lookup("TERM")
+	isDumb := !ok || term == dumbTerm
 	envp := colorProfile(isatty, environ)
 	if envp == TrueColor || envNoColor(environ) {
 		// We already know we have TrueColor, or NO_COLOR is set.
@@ -71,7 +71,8 @@ func Env(env []string) (p Profile) {
 }
 
 func colorProfile(isatty bool, env environ) (p Profile) {
-	isDumb := env.get("TERM") == dumbTerm
+	term, ok := env.lookup("TERM")
+	isDumb := !ok || term == dumbTerm
 	envp := envColorProfile(env)
 	if !isatty || isDumb {
 		// Check if the output is a terminal.

env_test.go
@@ -196,6 +196,20 @@ var cases = []struct {
 		},
 		expected: ANSI256,
 	},
+	{
+		name: "ignore COLORTERM when no TERM is defined",
+		environ: []string{
+			"COLORTERM=truecolor",
+		},
+		expected: func() Profile {
+			if runtime.GOOS == "windows" {
+				p, _ := windowsColorProfile(map[string]string{})
+				return p
+			} else {
+				return NoTTY
+			}
+		}(),
+	},
 }
 
 func TestEnvColorProfile(t *testing.T) {

commit b80753469fb11b9fa7866745e951a06a72ee2fb3

fix: windows check should happen earlier in colorProfile
Fixes: 96eea4a3b253 (fix: ignore COLORTERM when no TERM is defined)

env.go
@@ -72,7 +72,7 @@ func Env(env []string) (p Profile) {
 
 func colorProfile(isatty bool, env environ) (p Profile) {
 	term, ok := env.lookup("TERM")
-	isDumb := !ok || term == dumbTerm
+	isDumb := (!ok && runtime.GOOS != "windows") || term == dumbTerm
 	envp := envColorProfile(env)
 	if !isatty || isDumb {
 		// Check if the output is a terminal.